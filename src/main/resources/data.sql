57ade8752382b6f1423dd781bc680ef41aa0112c
Muhammed <muhammedihsansolak@gmail.com>
2023-11-17 11:18:14 +0300

    Sample user added to data.sql.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -17,15 +17,15 @@ VALUES ('2023-11-15 12:40:45', 1, false, '2023-11-15 12:40:45', 1, true, 'Mike',
         'ozzy@employee.com', 'MALE', 'abc1', '5574563533', 3);
 
 
-INSERT INTO projects(id, insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id,
+INSERT INTO projects( insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id,
                      end_date, start_date, project_code, project_detail, project_name, project_status, manager_id)
-VALUES (1, '2023-11-15 12:00:00', 1, false, '2023-11-15 12:00:00', 1, '2023-10-31', '2023-03-31', 'PROJ001',
+VALUES ( '2023-11-15 12:00:00', 1, false, '2023-11-15 12:00:00', 1, '2023-10-31', '2023-03-31', 'PROJ001',
         'Spring Core', 'Spring Core Project', 'OPEN', 2),
-       (2, '2023-11-15 12:30:00', 1, false, '2023-11-15 12:30:00', 1, '2023-08-22', '2023-05-01', 'PROJ002',
+       ( '2023-11-15 12:30:00', 1, false, '2023-11-15 12:30:00', 1, '2023-08-22', '2023-05-01', 'PROJ002',
         'Spring Boot', 'Spring Boot Project', 'IN_PROGRESS', 2),
-       (3, '2023-11-15 13:00:00', 1, false, '2023-11-15 13:00:00', 1, '2023-12-31', '2023-10-10', 'PROJ003',
+       ( '2023-11-15 13:00:00', 1, false, '2023-11-15 13:00:00', 1, '2023-12-31', '2023-10-10', 'PROJ003',
         'Spring MVC', 'Spring MVC Project', 'IN_PROGRESS', 3),
-       (4, '2023-11-15 13:00:00', 1, false, '2023-11-15 13:00:00', 1, '2023-12-31', '2023-10-10', 'PROJ004',
+       ( '2023-11-15 13:00:00', 1, false, '2023-11-15 13:00:00', 1, '2023-12-31', '2023-10-10', 'PROJ004',
         'Spring Data', 'Spring Data Project', 'COMPLETE', 3);
 
 INSERT INTO tasks(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id,
